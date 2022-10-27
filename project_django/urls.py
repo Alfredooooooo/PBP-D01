b147e735f2d36bb739b806270af94d67f4502f34
febidamanik <febidamanik@gmail.com>
2022-10-27 15:57:45 +0700

    add admin

--- project_django/urls.py
+++ project_django/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recycle.urls')),
+    path('admin/', include('admin.urls')),
 ]
 
 # TODO: Implement Routings Here
