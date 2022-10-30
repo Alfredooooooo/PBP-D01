a9be42b2d6858bfea8c637ec75c1681811726cfe
Alfredo <alfredo.austin@ui.ac.id>
2022-10-30 13:00:02 +0700

    finish my part

--- recycle/urls.py
+++ recycle/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
-from recycle.views import index, about_us, objectives, event, login, logout, register
+from recycle.views import index, about_us, objectives, event, login_user, logout_user, register_user, show_json_by_user, show_json_not_by_user, show_json_all, create_question, edit_question, delete_question
 
 app_name = 'recycle'
 
 urlpatterns = [
     path('', index, name='index'),
     path('aboutus/', about_us, name='about-us'),
-    path('objectives/', objectives, name='objectives'),
     path('event/', event, name='event'),
-    path("login/", login, name="login"),
-    path("logout/", logout, name="logout"),
-    path("register/", register, name="register"),
+    path("login/", login_user, name="login"),
+    path("logout/", logout_user, name="logout"),
+    path("register/", register_user, name="register"),
+    path("json-question-user/", show_json_by_user, name="jsonuser"),
+    path("json-question-notuser/", show_json_not_by_user, name="jsonnotuser"),
+    path("json-question-all/", show_json_all, name="jsonall"),
+    path("create-question/", create_question, name="createquestion"),
+    path("edit-question/<int:id>/", edit_question, name="editquestion"),
+    path("delete-question/<int:id>/", delete_question, name="deletequestion")
 ]
