c8ca8380d0285fe4598426c60346e957ec6b9453
Muflih Maxi <muflihmaxi@gmail.com>
2022-10-31 11:08:37 +0700

    laptop rusak

--- forum/views.py
+++ forum/views.py
@@ -4,12 +4,14 @@ from django.http import HttpResponse, HttpResponseNotFound
 from event.models import Event
 from forum.models import Comment
 import datetime
+from .forms import CommentForm
 
 def show_forum(request, id):
     data_forum = Event.objects.get(id=id)
     context = {
         'forum_data': data_forum,
         'logged_in': request.user.is_authenticated,
+        'form': CommentForm,
     }
     return render(request, 'forum.html', context)
 
@@ -20,23 +22,27 @@ def get_comments_json(request, id):
 
 def add_comment(request, id1, id2):
     if request.method == 'POST' and request.user.is_authenticated:
-        new_comment = Comment()
-        new_comment.user = request.user
-        new_comment.username = request.user.username
-        new_comment.date = str(datetime.datetime.now())
-        new_comment.comment_text = request.POST.get('comment_text')
-        if(id2 == 0):
-            new_comment.parent = None
-        else:
-            new_comment.parent = Comment.objects.get(pk=id2)
-            new_comment.parent_pk = new_comment.parent.pk
-            new_comment.parent_username = new_comment.parent.username
-        
-        event = Event.objects.get(id=id1)
-        new_comment.event = event
 
-        new_comment.save()
-        return HttpResponse(b"CREATED", status=201)
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            new_comment = Comment()
+            new_comment.user = request.user
+            new_comment.username = request.user.username
+            new_comment.date = str(datetime.datetime.now())
+            new_comment.comment_text = form.cleaned_data['comment_text']
+            if(id2 == 0):
+                new_comment.parent = None
+            else:
+                new_comment.parent = Comment.objects.get(pk=id2)
+                new_comment.parent_pk = new_comment.parent.pk
+                new_comment.parent_username = new_comment.parent.username
+            
+            event = Event.objects.get(id=id1)
+            new_comment.event = event
+
+            new_comment.save()
+            return HttpResponse(b"CREATED", status=201)
 
     return HttpResponseNotFound()
 
