bb1e42fbe680fe5bd5bbe791d749c8210601f51a
febidamanik <febidamanik@gmail.com>
2022-11-02 13:12:04 +0700

    add some changes in adminpage

--- adminpage/admin.py
+++ adminpage/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from.models import BarangWishlist
+from adminpage.models import BarangWishlist
+
 # Register your models here.
 admin.site.register(BarangWishlist)
--- adminpage/views.py
+++ adminpage/views.py
@@ -3,41 +3,43 @@ from adminpage.models import BarangWishlist
 from django.http import HttpResponse, HttpResponseNotFound
 from django.core import serializers
 from adminpage.forms import TaskForm
-from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/recycle/login')
 def wishlist(request):
     return render(request, "adminpage.html")
 
+@login_required(login_url='/recycle/login')
 def get_wishlist_json(request):
     wishlist_item = BarangWishlist.objects.all()
     return HttpResponse(serializers.serialize('json', wishlist_item))
 
+@login_required(login_url='/recycle/login')
 def add_wishlist_item(request):
     if request.method == 'POST':
         nama_barang = request.POST.get("nama_barang")
         harga_barang = request.POST.get("harga_barang")
         deskripsi = request.POST.get("deskripsi")
-        new_barang = BarangWishlist(nama_barang=nama_barang, harga_barang=harga_barang, deskripsi=deskripsi)
+        new_barang = BarangWishlist(user=request.user, nama_barang=nama_barang, harga_barang=harga_barang, deskripsi=deskripsi)
         new_barang.save()
 
         return HttpResponse(b"CREATED", status=201)
 
     return HttpResponseNotFound()
 
+@login_required(login_url='/recycle/login')
 def create_task(request):
     if request.method == "POST":
         form = TaskForm(request.POST)
-        if form.is_valid(): # Kondisi data pada field valid
-            task = BarangWishlist(
-                nama_barang = form.cleaned_data['nama_barang'], 
-                deskripsi = form.cleaned_data['deskripsi'],
-            )
-            task.save() # Menyimpan task ke database
-            return HttpResponseRedirect(reverse("adminpage:adminpage"))
+        if form.is_valid():
+            
+            return render(request, "adminpage.html")
     else:
         form = TaskForm()
     
     context = {'form':form}
     return render(request, "adminpage.html", context)
+
+
