69fd3dbfeb6440751962b28c53f548221ff469b4
unknown <muflihmaxi@gmail.com>
2022-12-04 04:33:03 +0700

    Add function to retrieve event JSON

--- forum/urls.py
+++ forum/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from forum.views import show_forum, get_comments_json, add_comment, delete_comment
+from forum.views import show_forum, get_comments_json, add_comment, delete_comment, get_event_json
 
 app_name = 'forum'
 
 urlpatterns = [
     path('<int:id>/', show_forum, name='show_forum'),
     path('json/<int:id>/', get_comments_json, name='get_comments_json'),
+    path('json/event/<int:id>/', get_event_json, name='get_event_json'),
     path('add/<int:id1>/<int:id2>/', add_comment, name='add_comment'),
     path('delete/<int:id>/', delete_comment, name='delete_comment'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -6,6 +6,9 @@ from forum.models import Comment
 import datetime
 from .forms import CommentForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+import json
+
 
 def show_forum(request, id):
     data_forum = Event.objects.get(id=id)
@@ -16,10 +19,23 @@ def show_forum(request, id):
     }
     return render(request, 'forum.html', context)
 
+
 def get_comments_json(request, id):
     event = Event.objects.get(id=id)
     comments = Comment.objects.filter(event=event)
-    return HttpResponse(serializers.serialize('json', comments))
+    return HttpResponse(serializers.serialize('json', comments), content_type="application/json")
+
+
+def get_event_json(request, id):
+    event = Event.objects.get(id=id)
+    username = event.user.username
+    return HttpResponse(json.dumps({'username': username,
+                                    'title': event.title,
+                                    'start_date': event.start_date.isoformat(),
+                                    'finish_date': event.finish_date.isoformat(),
+                                    'description': event.description}),
+                        content_type="application/json")
+
 
 @login_required(login_url='/login/')
 def add_comment(request, id1, id2):
@@ -33,13 +49,13 @@ def add_comment(request, id1, id2):
             new_comment.username = request.user.username
             new_comment.date = str(datetime.datetime.now())
             new_comment.comment_text = form.cleaned_data['comment_text']
-            if(id2 == 0):
+            if (id2 == 0):
                 new_comment.parent = None
             else:
                 new_comment.parent = Comment.objects.get(pk=id2)
                 new_comment.parent_pk = new_comment.parent.pk
                 new_comment.parent_username = new_comment.parent.username
-            
+
             event = Event.objects.get(id=id1)
             new_comment.event = event
 
@@ -47,6 +63,7 @@ def add_comment(request, id1, id2):
             return HttpResponse(b"CREATED", status=201)
 
     return HttpResponseNotFound()
+
 
 def delete_comment(request, id):
     if request.method == 'POST':
