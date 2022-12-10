464d8fa8f3b05e69b074f7137af8d136f6b664ce
unknown <muflihmaxi@gmail.com>
2022-12-10 18:28:21 +0700

    removed get_username function

--- forum/urls.py
+++ forum/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('json/event/<int:id>/', get_event_json, name='get_event_json'),
     path('add/<int:id1>/<int:id2>/', add_comment, name='add_comment'),
     path('delete/<int:id>/', delete_comment, name='delete_comment'),
-    path('username/', get_username, name='get_username'),
 ]
